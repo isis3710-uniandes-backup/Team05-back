0b3b7fefd61496b562014ab0870afa24763ae05c
Juliana Jaime <julianajaime@Julianas-MacBook-Pro.local>
2019-03-03 23:49:06 -0500

    Coleccion de postman de especimen

---
+++ routes/lugar.js
@@ -0,0 +1,57 @@
+let Lugar = require('../models/lugar');
+let express = require('express');
+let router = express.Router();
+
+router.route('/lugares').get(function(req, res) {
+  Clase.find(function(err, lugares) {
+    if (err) {
+      return res.send(err);
+    }
+
+    res.json(lugares);
+  });
+});
+
+router.route('/lugares').post(function(req, res) {
+  let lugar = new Lugar(req.body);
+
+  lugar.save(function(err) {
+    if (err) {
+      return res.send(err);
+    }
+
+    res.send({ message: 'Lugar añadido' });
+  })
+});
+
+router.route('/lugares/:id').put(function(req, res) {
+  Lugar.findOne({ _id: req.params.id }, function(err, lugar) {
+    if (err) {
+      return res.send(err);
+    }
+
+    for (prop in req.body) {
+      lugar[prop] = req.body[prop];
+    }
+
+    lugar.save(function(err) {
+      if (err) {
+        return res.send(err);
+      }
+
+      res.send({ message: 'Lugar actualizado'});
+    });
+  })
+});
+
+router.route('/lugares/:id').delete(function(req, res) {
+  Clase.deleteOne({ _id: req.params.id }, function(err, lugar) {
+    if (err) {
+      return res.send(err);
+    }
+
+    res.json({ message: 'Lugar eliminado' });
+  });
+});
+
+module.exports = router;
